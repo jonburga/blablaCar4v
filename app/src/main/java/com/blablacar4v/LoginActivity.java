8c6f3571ff0c5be433329c869c71d41729f0839b
asier <suarexasier@gmail.com>
2024-02-27 21:30:30 +0100

    Cambios: interfaz para login funcional!

---
+++ app/src/main/java/com/blablacar4v/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.blablacar4v;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.security.Provider;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAnalytics analytics;
+    Bundle bundle;
+
+    EditText emailEditText;
+    EditText passwordEditText;
+    TextView singUpButton;
+    Button loginInButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginInButton = findViewById(R.id.loginInButton);
+        singUpButton = findViewById(R.id.textViewSignUp);
+
+        analytics = FirebaseAnalytics.getInstance(this);
+        bundle = new Bundle();
+        bundle.putString("message", "Integración de Firebase completada");
+        analytics.logEvent("InitScreen", bundle);
+        setUp();
+
+        //SetUp
+
+    }
+    private void setUp(){
+        singUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!emailEditText.getText().toString().isEmpty() ||! passwordEditText.getText().toString().isEmpty()){
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString());
+                    if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                        showHome(FirebaseAuth.getInstance().getCurrentUser().getEmail(), ProviderType.BASIC);
+                    }
+                }else{
+                    showAlert();
+                }
+            }
+        });
+
+        loginInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!emailEditText.getText().toString().isEmpty() ||! passwordEditText.getText().toString().isEmpty()){
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString());
+                    if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                        showHome(FirebaseAuth.getInstance().getCurrentUser().getEmail(), ProviderType.BASIC);
+                    }
+                }else{
+                    showAlert();
+                }
+            }
+        });
+    }
+
+    private void showAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage("Se ha producido un error autenticando el usuario");
+        builder.setPositiveButton("Aceptar", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void showHome(String email, ProviderType provider){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("email", email);
+        intent.putExtra("provider", provider.name());
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/blablacar4v/MainActivity.java
+++ app/src/main/java/com/blablacar4v/MainActivity.java
@@ -3,12 +3,36 @@ package com.blablacar4v;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+enum ProviderType {
+    BASIC;
+}
 public class MainActivity extends AppCompatActivity {
-
+    Bundle bundle;
+    TextView tx;
+    Button logoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bundle = getIntent().getExtras();
+        tx = findViewById(R.id.textView5);
+        logoutButton = findViewById(R.id.logoutButton);
+        String email = bundle.getString("email");
+        String provider = bundle.getString("provider");
+
+        setUp(email, ProviderType.valueOf(provider));
     }
+    private void setUp(String email, ProviderType provider){
+        tx.setText(email);
+        logoutButton.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            onBackPressed();
+                });
+    }
+
 }
