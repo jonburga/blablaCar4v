2a202ce62aa32923a085c175cbbcb063f4891aef
asier <suarexasier@gmail.com>
2024-02-28 19:11:02 +0100

    Cambios: Creada activity para crear un viaje no funcional

--- app/src/main/java/com/blablacar4v/models/User.java
+++ app/src/main/java/com/blablacar4v/models/User.java
@@ -11,7 +11,7 @@ public class User {
     public User() {
     }
 
-    public  User(String email, String password){
+    public User(String email, String password){
         this.email = email;
         this.password = password;
     }
