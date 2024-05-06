daa5e4dd68acb4c62779e9a2ab4db419ee22bcb2
Jon burga <jonanderburga@gmail.com>
2024-05-06 20:30:16 +0200

    validation email

--- app/src/main/java/com/blablacar4v/SignUpActivity.java
+++ app/src/main/java/com/blablacar4v/SignUpActivity.java
@@ -13,6 +13,9 @@ import com.blablacar4v.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class SignUpActivity extends AppCompatActivity {
     private FirebaseFirestore db;
@@ -38,32 +41,33 @@ public class SignUpActivity extends AppCompatActivity {
         setUp();
     }
 
-    private  void setUp(){
+    private void setUp() {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*
                 if (!emailEditText.getText().toString().isEmpty() || !passwordEditText.getText().toString().isEmpty()) {
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString());
-                          db.("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(new User(
-                                directionEditText.getText().toString(),
-                                passwordEditText.getText().toString(),
-                                emailEditText.getText().toString(),
-                                nameEditText.getText().toString(),
-                                phoneEditText.getText().toString(),
-                                ""
-                        ));
-                        showHome(FirebaseAuth.getInstance().getCurrentUser().getEmail(), ProviderType.BASIC);
-                    }
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                            .addOnSuccessListener(authResult -> {
+                                db.collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(new User(
+                                        emailEditText.getText().toString(),
+                                        passwordEditText.getText().toString(),
+                                        directionEditText.getText().toString(),
+                                        phoneEditText.getText().toString(),
+                                        nameEditText.getText().toString()
+                                ));
+                                showHome(FirebaseAuth.getInstance().getCurrentUser().getEmail(), ProviderType.BASIC);
+                            })
+                            .addOnFailureListener(e -> {
+                                // Manejar fallo en la creación del usuario
+                                Toast.makeText(SignUpActivity.this, "Error en el registro: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            });
                 } else {
                     showAlert();
                 }
-                 */
-
             }
-
         });
     }
+
     private void showHome(String name, ProviderType provider){
         Intent intent = new Intent(this, LoginActivity.class);
         intent.putExtra("name", name);
@@ -72,5 +76,31 @@ public class SignUpActivity extends AppCompatActivity {
     }
     private void showAlert(){
         Toast.makeText(this, "Se ha producido un error", Toast.LENGTH_SHORT).show();
+    }
+    private boolean validateDomain(String validEmail) {
+        String[] validDomains = {"gmail.com", "hotmail.com"};
+
+        String[] emailParts = validEmail.split("@");
+        if (emailParts.length == 2) {
+            String domain = emailParts[1].toLowerCase();
+
+            for (String validDomain : validDomains) {
+                if (domain.equals(validDomain)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean validateEmail(String email) {
+        String emailPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+        Pattern pattern = Pattern.compile(emailPattern);
+
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
     }
 }
